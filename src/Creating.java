3cb733a3448c7f7ad1cf26ea2f7708dafbe71f1c
Ayush-pailoss <pailossayush@gmail.com>
2024-01-31 20:33:55 +0530

    arrayOP

--- src/Creating.java
+++ src/Creating.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
 
 public class Creating {
+   static int count =0;
+    Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner  scanner = new Scanner(System.in);
-        int [] a= new int[5];
-        try{
-        for (int i = 0; i <a.length ; i++) {
-            System.out.println("enter number  :");
-            a[i] = scanner.nextInt();
+        int [] array = new int[5];
+        for (int i = 0; i <array.length ; i++) {
+            Creating.insert(array,i+1);
+        }
+        Creating.insertAt(array,26);
+        Creating.insertAt(array,103);
+        Creating.insertAtPosition(array,3,33);
+        Creating.print(array);
 
+//        for(int j : array){
+//            System.out.print(j+" ");
+//        }
+
+    }
+    public static void insertAtPosition(int[]a, int position,int val){
+        for (int i =count-1; i>position ; i--) {
+        a[i]=a[i-1];
         }
+        a[position]=val;
+    }
+    public static void insert(int[] index, int val){
+        index[count++]=val;
+    }
+    public static void print(int [] a){
+        for (int j = 0; j < a.length ; j++) {
+            System.out.print(a[j] + " ");
         }
-        catch (IndexOutOfBoundsException ex){
-            System.out.println("index out of bound : ");
+    }
+    public static void insertAt(int ar [], int val){
+        for (int i =count-1;i>0;i-- ) // in place count we can write ar.length
+        {
+            ar[i]=ar[i-1];
         }
-        for (int j=0;j<=a.length;j++){
-            System.out.println(a[j]);
-        }
+        ar[0]=val;
     }
 }
